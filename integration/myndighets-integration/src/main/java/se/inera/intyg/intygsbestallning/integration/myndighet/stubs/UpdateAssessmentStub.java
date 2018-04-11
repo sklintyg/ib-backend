75b5b66a2ce9cae5a86f56dbf35ea3c700c6c8fe
an3ll <jocke.anell@gmail.com>
2018-04-11 17:14:53 +0200

    DEVENV: checkstyle fixes

--- integration/myndighets-integration/src/main/java/se/inera/intyg/intygsbestallning/integration/myndighet/stubs/UpdateAssessmentStub.java
+++ integration/myndighets-integration/src/main/java/se/inera/intyg/intygsbestallning/integration/myndighet/stubs/UpdateAssessmentStub.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.inera.intyg.intygsbestallning.integration.myndighet.stubs;
 
 import org.slf4j.Logger;
@@ -16,12 +34,14 @@ import java.time.format.DateTimeFormatter;
 public class UpdateAssessmentStub implements UpdateAssessmentResponderInterface {
 
     private static final Logger LOG = LoggerFactory.getLogger(UpdateAssessmentStub.class);
+    private static final long DAYS = 7L;
 
     @Override
     public UpdateAssessmentResponseType updateAssessment(String s, UpdateAssessmentType updateAssessmentType) {
+
         LOG.info("UpdateAssessmentStub received request");
         UpdateAssessmentResponseType response = new UpdateAssessmentResponseType();
-        response.setLastDateForCertificateReceival(LocalDateTime.now().plusDays(7L).format(DateTimeFormatter.ISO_DATE));
+        response.setLastDateForCertificateReceival(LocalDateTime.now().plusDays(DAYS).format(DateTimeFormatter.ISO_DATE));
         ResultType rt = new ResultType();
         rt.setResultCode(ResultCodeType.OK);
         response.setResult(rt);
