efa6fad310b9c04413cd1195f0399e725dad8490
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-04-16 17:38:30 +0200

    DEVENV: Lade till exempeltest för ForfraganController

--- web/src/test/java/se/inera/intyg/intygsbestallning/web/controller/api/ForfraganControllerIT.java
+++ web/src/test/java/se/inera/intyg/intygsbestallning/web/controller/api/ForfraganControllerIT.java
@@ -1,15 +1,23 @@
 package se.inera.intyg.intygsbestallning.web.controller.api;
 
+import com.jayway.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import se.inera.intyg.intygsbestallning.web.BaseRestIntegrationTest;
 
+import static com.jayway.restassured.RestAssured.given;
 import static org.junit.Assert.assertTrue;
 
 public class ForfraganControllerIT extends BaseRestIntegrationTest {
 
+    private static final String FORFRAGAN_API_ENDPOINT = "/api/forfragningar" ;
+
     @Test
     public void testGetAnvandare() {
         assertTrue(true);
+        RestAssured.sessionId = getAuthSession(DEFAULT_VARDADMIN);
+        given().expect().statusCode(OK).when().get(FORFRAGAN_API_ENDPOINT)
+        .then().body("forfragningar", Matchers.notNullValue());
 //        RestAssured.sessionId = getAuthSession(DEFAULT_VARDADMIN);
 //        given().expect().statusCode(OK).when().get(USER_API_ENDPOINT).
 //                then().
