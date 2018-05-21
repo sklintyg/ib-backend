9647a327a14bd96a5b3240ae4db86aaff68b26ee
Benny Samuelsson <benny@solidcore.se>
2018-05-21 15:46:48 +0200

    INTYG-6217: Checkstyle.

--- web/src/main/java/se/inera/intyg/intygsbestallning/web/controller/testability/RegistreradVardenhetResource.java
+++ web/src/main/java/se/inera/intyg/intygsbestallning/web/controller/testability/RegistreradVardenhetResource.java
@@ -21,19 +21,14 @@ package se.inera.intyg.intygsbestallning.web.controller.testability;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import se.inera.intyg.intygsbestallning.common.exception.IbNotFoundException;
 import se.inera.intyg.intygsbestallning.persistence.model.RegistreradVardenhet;
-import se.inera.intyg.intygsbestallning.persistence.model.Utredning;
 import se.inera.intyg.intygsbestallning.persistence.repository.RegistreradVardenhetRepository;
-import se.inera.intyg.intygsbestallning.persistence.repository.UtredningRepository;
 
 import javax.ws.rs.core.Response;
 
