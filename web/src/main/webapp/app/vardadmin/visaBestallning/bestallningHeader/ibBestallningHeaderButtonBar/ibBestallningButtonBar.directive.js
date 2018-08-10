4507cffba67bc1b3c49b2f5d4278d16211211f5a
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2018-08-10 13:45:59 +0200

    INTYG-6747: Punkt 2, man skall kunna registrera mottagen handling även i status UTREDNING_PAGAR

--- web/src/main/webapp/app/vardadmin/visaBestallning/bestallningHeader/ibBestallningHeaderButtonBar/ibBestallningButtonBar.directive.js
+++ web/src/main/webapp/app/vardadmin/visaBestallning/bestallningHeader/ibBestallningHeaderButtonBar/ibBestallningButtonBar.directive.js
@@ -53,7 +53,8 @@ angular.module('ibApp').directive('ibBestallningButtonBar',
 
                 $scope.registerReceivedHandlingDisabled = function () {
                     if ($scope.bestallning.status.id === 'BESTALLNING_MOTTAGEN_VANTAR_PA_HANDLINGAR' ||
-                        $scope.bestallning.status.id === 'UPPDATERAD_BESTALLNING_VANTAR_PA_HANDLINGAR') {
+                        $scope.bestallning.status.id === 'UPPDATERAD_BESTALLNING_VANTAR_PA_HANDLINGAR' ||
+                        $scope.bestallning.status.id === 'UTREDNING_PAGAR') {
                         return false;
                     }
                     return true;
